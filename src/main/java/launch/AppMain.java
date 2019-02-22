a1c14ad9210c603245dc17925537c99fe464efec
ccxy0070 <30308342+ccxy0070@users.noreply.github.com>
2019-02-22 09:07:25 +0800

    boot

--- src/main/java/launch/AppMain.java
+++ src/main/java/launch/AppMain.java
@@ -1,6 +1,7 @@
 package launch;
 
 import tomcat.run.impl.V1Start;
+import tomcat.run.impl.V2Start;
 
 public class AppMain {
 
@@ -8,10 +9,8 @@ public class AppMain {
 	private final static String HOSTHOME = "localhost";
 	
 	public static void main(String[] args) throws Throwable {
-		
-		new V1Start().start(PORT,HOSTHOME,AppMain.class);
-		//new V2Start().start(PORT,HOSTHOME,AppMain.class);
-		
+		//new V1Start().start(PORT,HOSTHOME,AppMain.class);
+		new V2Start().start(PORT,HOSTHOME,AppMain.class);
 	}
 		
 }
